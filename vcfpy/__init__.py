104a4657e09b2ef25d4376874e05ef33d9dccf93
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2016-09-19 11:17:22 +0200

    Adding `vcfpy.__init__`

--- vcfpy/__init__.py
+++ vcfpy/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+__version__ = '0.1.0'
+
 from .exceptions import VCFPyException, InvalidHeaderException, \
     InvalidRecordException, IncorrectVCFFormat, HeaderNotFound
 
