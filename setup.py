cc012b87a0170a5ea4822a7562a1e519e8942961
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2016-09-19 12:20:58 +0200

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='vcfpy',
-    version='0.2.0',
+    version='0.2.1',
     description="Python 3 VCF parser to circumvent problems in PyVCF",
     long_description=readme + '\n\n' + history,
     author="Manuel Holtgrewe",
