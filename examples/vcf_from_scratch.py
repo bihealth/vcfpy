8ef9aedbb560f16164183fba29c059bc6940d264
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2020-01-22 13:02:25 +0100

    Adding example on how to create a VCF from scratch without samples.

---
+++ examples/vcf_from_scratch.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Create VCF from scratch using vcfpy."""
+
+import argparse
+import itertools
+import sys
+import time
+
+import vcfpy
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: vcf_from_scratch.py OUTPUT.vcf", file=sys.stderr)
+        return 1
+
+    header = vcfpy.Header(samples=vcfpy.SamplesInfos([]))
+    with vcfpy.Writer.from_path(sys.argv[1], header) as writer:
+        record = vcfpy.Record(
+            CHROM="1", POS=1, ID=[], REF="N", ALT=[], QUAL=None, FILTER=[], INFO={}, FORMAT=[]
+        )
+        writer.write_record(record)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
