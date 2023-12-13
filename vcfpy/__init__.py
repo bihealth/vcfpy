a6f8af4a8da2bf8b695d0486ece7a609ca7e076f
Chris Markiewicz <effigies@gmail.com>
2023-12-13 15:06:06 -0500

    ENH: Upgrade versioneer to latest
    Python 3.12 removed legacy components

--- vcfpy/__init__.py
+++ vcfpy/__init__.py
@@ -47,7 +47,6 @@ from .reader import Reader
 
 from .writer import Writer
 
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
+del _version
